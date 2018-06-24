80050af636de9cc6dae7b8538dcb213e3cf128b3
Rob Tarr <Rob@robtarr.net>
2018-06-23 21:50:14 -0400

    fix: Copy .env on deploy

--- shipitfile.js
+++ shipitfile.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = function(shipit) {
   require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
 
   var deployPath = '/usr/src/house-hunter';
 
@@ -11,9 +12,13 @@ module.exports = function(shipit) {
       deployTo: deployPath,
       repositoryUrl: 'git@github.com:robtarr/house-hunter.git',
       ignores: ['.git', 'node_modules'],
-      keepReleases: 2,
+      keepReleases: 4,
       key: '/Users/Rob/.ssh/id_rsa',
-      shallowClone: false
+      shallowClone: false,
+      shared: {
+        baseDir: 'shared',
+        files: ['.env']
+      }
     },
     production: {
       servers: `${process.env.deployUser}@${process.env.deployServer}`
